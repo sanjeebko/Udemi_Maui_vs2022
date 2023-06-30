c35bf984bf1fe791a5a3e408c187624d906c04c7
sanjeeb <sanjeebojha@gmail.com>
2023-06-30 16:04:10 +0100

    Rest demo completed

---
+++ RestDemo/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RestDemo.MVVM.ViewModels;
+
+namespace RestDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
